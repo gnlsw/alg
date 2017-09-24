4239128dd2dee04e3708e3375c3ee4fc14245f07
gnlsw <today_wsl@163.com>
2017-09-24 12:54:36 +0800

    增加选择排序

---
+++ books/algorithm-4th/ch02/Selection.java
@@ -0,0 +1,67 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+
+public class Selection
+{
+    public static void sort(Comparable[] a)
+    {
+        // 将a[]按升序进行排列
+        int N = a.length;    // 数组的长度
+        for(int i = 0; i < N; i++)
+        {
+            // 将a[i]和a[i+1 ... N]中最小的元素交换
+            int min = i;    // 最小元素的索引
+            for(int j = i + 1; j < N; j++)
+            {
+                if(less(a[j], a[min]))
+                {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w)
+    {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j)
+    {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a)
+    {
+        // 在单行中打印数组
+        for(int i = 0; i < a.length; i++)
+        {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a)
+    {
+        for(int i = 1; i < a.length; i++)
+        {
+            if(less(a[i], a[i-1]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        // 从标准输入读取字符串，将它们排序并输出
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
