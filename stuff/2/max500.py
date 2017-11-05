dad46f9a76f7e675bf982377861b6372d91c6cb1
gnlsw <today_wsl@163.com>
2017-11-05 14:40:19 +0800

    增加堆操作的实现，修改测试数据

--- stuff/2/max500.py
+++ stuff/2/max500.py
@@ -1,5 +1,42 @@
-def add_record(record):
-    print record
+def sift_down(result):
+    heap_length = len(result)
+    index = 1
+    while index * 2 < heap_length:
+        child_index = index * 2
+        if child_index + 1 < heap_length:
+            if result[child_index].compare_count(result[child_index + 1]) > 0:
+                child_index += 1
+
+        if result[index].compare_count(result[child_index]) > 0:
+            tmp = result[index]
+            result[index] = result[child_index]
+            result[child_index] = tmp
+            index = child_index
+        else:
+            break
+
+
+def sift_up(result):
+    index = len(result) - 1
+    while(index > 1):
+        if(result[index].compare_count(result[index/2]) < 0):
+            tmp = result[index]
+            result[index] = result[index/2]
+            result[index / 2] = tmp
+            index = index / 2
+        else:
+            break
+    return
+
+def add_record(result, record):
+    if len(result) < 8:
+        result.append(record)
+        sift_up(result);
+    else:
+        # compare the reocrd's count with the item[1]'s count
+        # if bigger, the replace
+        result[1] = record
+        sift_down(result)
 
 class Record:
     def __init__(self, time):
@@ -12,14 +49,20 @@ class Record:
         else:
             return False
 
-    def count_plus_one():
+    def count_plus_one(self):
         self.count += 1
+
+    def compare_count(self, record):
+        return self.count - record.count
 
     def __str__(self):
         return 'time is {0}, count is {1}'.format(self.time, self.count)
 
 
 if __name__ == "__main__":
+    result = []
+    result.append('None')
+
     fp_in = open("trade.log", 'r')
 
     # read first line
@@ -32,7 +75,7 @@ if __name__ == "__main__":
         if current_record.same_time(next_time):
             current_record.count_plus_one()
         else:
-            add_record(current_record)
+            add_record(result, current_record)
             current_record = Record(next_time)
 
-    add_record(current_record)
+    add_record(result, current_record)
